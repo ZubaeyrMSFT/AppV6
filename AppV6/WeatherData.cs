045879f12d6c41555d2df868b9ba02dec2f8f4cc
Zubairuddin Mohammed <zmohammed@microsoft.com>
2021-07-30 03:32:28 +0530

    Add project files.

---
+++ AppV6/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AppV6
+{
+    public class WeatherData
+    {
+
+        public String LocationName { get; set; }
+
+        public Int32 EventDate { get; set; }
+
+        public Double Rainfall { get; set; }
+
+    }
+}
